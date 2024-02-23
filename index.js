91fcfa95b7d9957cd564e4b6ecb050a9a09c056e
sergio castano <sergiocastano@Sergios-MBP.local>
2024-02-23 08:27:36 -0500

    Const port update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import dotenv from 'dotenv';
 
 const app = express();
-const port = 10000;
+const port = process.env.PORT || 10000;
 const API_SunriseSunset_URL = "https://api.sunrise-sunset.org/json?";
 
 // Retrieve API keys
